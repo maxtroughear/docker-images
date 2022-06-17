aaa17e09e74fe086d950b9274c62d44c085bc19d
Max Troughear <max@maxtroughear.dev>
2022-06-17 18:50:45 +1200

    Build sogo against ubuntu jammy for libwbxml2-1

--- mailcow-dockerized-arm64/Dockerfiles/sogo/build.sh
+++ mailcow-dockerized-arm64/Dockerfiles/sogo/build.sh
@@ -32,7 +32,7 @@ SOGO_GIT_TAG="SOGo-${VERSION_TO_BUILD}"
 SOPE_GIT_TAG="SOPE-${VERSION_TO_BUILD}"
 
 PACKAGES_DIR="${BASE_DIR}/vendor"
-PACKAGES_TO_INSTALL="git zip wget make debhelper gnustep-make libssl-dev libgnustep-base-dev libldap2-dev libytnef0-dev zlib1g-dev libpq-dev libmariadbclient-dev-compat libmemcached-dev liblasso3-dev libcurl4-gnutls-dev devscripts libexpat1-dev libpopt-dev libsbjson-dev libsbjson2.3 libcurl4 liboath-dev libsodium-dev libzip-dev libwbxml2-dev libwbxml2-0"
+PACKAGES_TO_INSTALL="git zip wget make debhelper gnustep-make libssl-dev libgnustep-base-dev libldap2-dev libytnef0-dev zlib1g-dev libpq-dev libmariadbclient-dev-compat libmemcached-dev liblasso3-dev libcurl4-gnutls-dev devscripts libexpat1-dev libpopt-dev libsbjson-dev libsbjson2.3 libcurl4 liboath-dev libsodium-dev libzip-dev libwbxml2-dev libwbxml2-1"
 
 export DEBIAN_FRONTEND=noninteractive
 
