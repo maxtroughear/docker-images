1afcb5d370a14ee3dd260ebf161fbacc19715bd1
Max Troughear <max@maxtroughear.dev>
2022-06-21 00:15:35 +1200

    Fix sope deb typo

--- mailcow-dockerized-arm64/Dockerfiles/sogo/build.sh
+++ mailcow-dockerized-arm64/Dockerfiles/sogo/build.sh
@@ -99,7 +99,7 @@ dpkg -i sogo_${VERSION_TO_BUILD}_*.deb
 # Copy required packages to output directory
 mkdir output
 cp libwbxml2-1_0.11.8+dfsg-1_${ARCH}.deb ./output
-cp libsope*4.9_*${ARCH}.dev ./output
+cp libsope*4.9_*${ARCH}.deb ./output
 cp sope4.9-gdl1-mysql_4.9.r1664_${ARCH}.deb ./output
 cp sope4.9-libxmlsaxdriver_4.9.r1664_${ARCH}.deb ./output
 cp sogo_${VERSION_TO_BUILD}_${ARCH}.deb ./output
